aad46c2372ecf47f56655a3dc6d325db5a8135a3
soosaithasandinesch-star <soosaithasandinesch@gmail.com>
2026-09-16 13:04:18 +0200

    Refactor _detect_subtype function
    Refactor _detect_subtype function to remove parameter.

--- maincode.py
+++ maincode.py
@@ -215,7 +215,7 @@ def get_clip_text_features():
     return feats
 
 
-def _detect_subty )pe(image_features, kategorie):
+def _detect_subty )pe():
     """Erkennt feinere Merkmale innerhalb der Kategorie (z. B. Hoodie vs. Jacke)."""
     import torch
     subtypes = SUBTYPE_PROMPTS.get(kategorie, [])
